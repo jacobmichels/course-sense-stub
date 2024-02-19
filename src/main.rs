3fd64defdd8388ac2da141187f2fef11b76ca93d
Jacob Michels <jacob.michels2025@gmail.com>
2024-02-18 21:10:52 -0500

    add logging improve error handling load .env

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
+use color_eyre::{eyre::Context, Result};
+use log::info;
 use std::env;
 
 use axum::{extract::Request, Router};
 
 #[tokio::main]
 async fn main() {
-    let port = env::var("PORT").unwrap_or("3000".to_string());
+    let _ = dotenvy::dotenv(); // ignoring the error if the .env file is not found
+    color_eyre::install().expect("failed to install color_eyre");
+    env_logger::init();
+
+    run().await.unwrap();
+}
+
+async fn run() -> Result<()> {
+    let port = env::var("PORT").wrap_err("$PORT not defined")?;
 
     let app = Router::new().fallback(handler);
 
-    println!("Server running on port {}", port);
+    info!("Server running on port {}", port);
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+        .wrap_err("failed to find to port: {port}")?;
+    axum::serve(listener, app)
+        .await
+        .wrap_err("failed to serve app")?;
+
+    Ok(())
 }
 
 async fn handler(req: Request) -> &'static str {
-    println!("Request received: {:?}", req);
+    info!("Request received: {:?}", req);
     "Hi, thanks for using Course Sense. Unfortunately, it's been discontinued. Thanks for your support."
 }
